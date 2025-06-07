ea23a92ba2bf3186a5b1e3f6539a7abe2ad4f3f7
un4rch <81522320+un4rch@users.noreply.github.com>
2025-06-07 19:05:58 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+TACO_DIR="DeepLearningExamples/PyTorch/SpeechSynthesis/Tacotron2/tacotron2"
+if [ ! -d "$TACO_DIR" ]; then
+    echo "Cloning Tacotron2 repository..."
+    git clone https://github.com/NVIDIA/DeepLearningExamples.git
+fi
+echo "Copying model.py to $TACO_DIR ..."
+mkdir -p "$TACO_DIR"
+cp -f model.py "$TACO_DIR"/
+
+FILE="train-clean-100.tar.gz"
+DIR="data/LibriTTS/"
+
+# Check if the file exists
+if [ ! -f "$FILE" ]; then
+    echo "$FILE not found. Downloading..."
+    wget https://www.openslr.org/resources/60/$FILE
+else
+    echo "$FILE already exists. Skipping download."
+fi
+
+# Extract if not already extracted
+if [ ! -d "${DIR}LibriTTS/train-clean-100" ]; then
+    mkdir -p "$DIR"
+    tar -xzvf "$FILE" -C "$DIR"
+else
+    echo "Data already extracted in $DIR. Skipping extraction."
+fi
+
+#rm "$FILE"  # Clean up the tar file after extraction
+
+python -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+python make_metadata.py
+
+python train.py
+
+python infer.py --checkpoint checkpoints/tacotron2_epoch9.pth --text "Hello, I am Unai" --speaker_wav data/my_voice/wavs/Voz1.wav data/my_voice/wavs/Voz2.wav data/my_voice/wavs/Voz3.wav --out_wav ex.wav
